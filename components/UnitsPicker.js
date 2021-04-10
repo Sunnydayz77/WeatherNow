2cea2e2a3e6642907aa56b228077426579d4faf4
Jennifer Piwonski <jenpiwonski@gmail.com>
2021-04-09 23:57:40 -0400

    added android styling and dropdown for unit picker

--- components/UnitsPicker.js
+++ components/UnitsPicker.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Platform } from 'react-native'
 import { Picker } from '@react-native-community/picker'
 
 export default function UnitsPicker({unitsSystem, setUnitsSystem}) {
     return (
         <View style={styles.unitsSystem}>
-            <Picker selectedValue={unitsSystem} onValueChange={(item) => setUnitsSystem(item) }>
+            <Picker selectedValue={unitsSystem} onValueChange={(item) => setUnitsSystem(item) } mode="dropdown">
                 <Picker.Item label="C°" value="metric"/>
                 <Picker.Item label="F°" value="imperial"/>
             </Picker>
@@ -16,7 +16,14 @@ export default function UnitsPicker({unitsSystem, setUnitsSystem}) {
 const styles = StyleSheet.create({
     unitsSystem: {
         position: 'absolute',
-        top: -20,
+        ... Platform.select({
+            ios: {
+                 top: -20,
+            },
+            android: {
+                top: 20,
+            },
+        }),
         left: 20,
         height: 50,
         width: 100
